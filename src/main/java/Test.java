e5e9c043e57882e7f7af760aad7f88656d92174f
周政卫 <14643151+zheng_wei18061094568@user.noreply.gitee.com>
2025-04-23 09:51:01 +0800

    add dev_lx

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Package PACKAGE_NAME.Test
+ * @Author zhengwei_zhou
+ * @Date 2025/4/23 9:48
+ * @description: Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
