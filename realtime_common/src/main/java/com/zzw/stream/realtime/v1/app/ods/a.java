8e4030df8abe92c201146efcdbb23716de896fab
周政卫 <14643151+zheng_wei18061094568@user.noreply.gitee.com>
2025-04-30 10:20:21 +0800

    add 4_30

---
+++ realtime_common/src/main/java/com/zzw/stream/realtime/v1/app/ods/a.java
@@ -0,0 +1,13 @@
+package com.zzw.stream.realtime.v1.app.ods;
+
+/**
+ * @Package com.zzw.stream.realtime.v1.app.ods.a
+ * @Author zhengwei_zhou
+ * @Date 2025/4/30 8:37
+ * @description: a
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println("zz省份");
+    }
+}
