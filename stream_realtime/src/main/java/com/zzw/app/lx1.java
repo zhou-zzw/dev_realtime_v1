65cc524d2111384bf43846f3a166dcc50068d73d
周政卫 <14643151+zheng_wei18061094568@user.noreply.gitee.com>
2025-05-23 22:03:50 +0800

    add 5_23

---
+++ stream_realtime/src/main/java/com/zzw/app/lx1.java
@@ -0,0 +1,13 @@
+package com.zzw.app;
+
+/**
+ * @Package com.zzw.app.lx1
+ * @Author zhengwei_zhou
+ * @Date 2025/5/23 9:03
+ * @description:
+ */
+public class lx1 {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+    }
+}
